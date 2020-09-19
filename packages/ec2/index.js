d1c9b376c10ebeee95355cb60f2833dbc3875508
Lauren <gorskilaurens@gmail.com>
2020-09-18 21:23:45 -0500

    pm2 setup

--- packages/ec2/index.js
+++ packages/ec2/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send("Hello world!")
+  res.send("Hello world we made it!")
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
