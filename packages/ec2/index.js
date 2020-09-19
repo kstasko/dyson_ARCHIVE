a6e04b1b9abd273eb5e31c8ed56986413b1b2fb8
Lauren <gorskilaurens@gmail.com>
2020-09-18 21:42:42 -0500

    write to discord

--- packages/ec2/index.js
+++ packages/ec2/index.js
@@ -1,6 +1,52 @@
 const express = require('express')
 const app = express()
+const Discord = require('discord.js');
+
+const bot = new Discord.Client();
 app.get('/', (req, res) => {
-  res.send("Hello world we made it!")
+    res.send("Hello world we made it!")
 })
-app.listen(3000, () => console.log('Server running on port 3000'))
+
+app.get('/dyson', (req, res) => {
+    secret = await getSecret();
+    bot.once('ready', () => {
+        console.log('At the ready!!')
+    });
+
+    bot.on("message", function (msg) {
+        if (msg.content === 'tight') {
+            msg.channel.send('tight.');
+            console.log("toight-ed " + msg.author.username);
+        }
+    });
+
+    bot.login(secret);
+    return { statusCode: 200, body: JSON.stringify("Hello from EC2!") }
+})
+
+async function getSecret() {
+    var AWS = require('aws-sdk'),
+        region = process.env.AWS_REGION,
+        secretName = 'bot_client_secret',
+        secret,
+        decodedBinarySecret;
+
+    var client = new AWS.SecretsManager({
+        region: region
+    });
+
+    const data = await client.getSecretValue({ SecretId: secretName }).promise();
+    if ('SecretString' in data) {
+        secret = JSON.parse(data.SecretString).bot_client_secret;
+        return secret;
+    }
+    else {
+        decodedBinarySecret = new Buffer(data.SecretBinary, 'base64').toString('ascii');
+        return decodedBinarySecret;
+    }
+
+};
+
+
+
+app.listen(3000, () => console.log('Server running on port 3000'));
