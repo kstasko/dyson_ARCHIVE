e4c837d7f2f263b4328a41da748a01d8ba15cdc2
Lauren <gorskilaurens@gmail.com>
2020-09-20 20:51:54 -0500

    hack = true

--- packages/lambda/index.js
+++ packages/lambda/index.js
@@ -1,43 +1,39 @@
 const Discord = require('discord.js');
 
 const bot = new Discord.Client();
+var AWS = require('aws-sdk');
+const region = process.env.AWS_REGION;
 
-async function getSecret() {
-  var AWS = require('aws-sdk'),
-    region = process.env.AWS_REGION,
-    secretName = 'bot_client_secret',
-    secret,
-    decodedBinarySecret;
+const client = new AWS.SecretsManager({
+  region: region
+});
 
-  var client = new AWS.SecretsManager({
-    region: region
-  });
-
+async function getSecret(secretName) {
   const data = await client.getSecretValue({ SecretId: secretName }).promise();
   if ('SecretString' in data) {
-    secret = JSON.parse(data.SecretString).bot_client_secret;
-    return secret;
+    return JSON.parse(data.SecretString)[secretName];
   }
   else {
-    decodedBinarySecret = new Buffer(data.SecretBinary, 'base64').toString('ascii');
-    return decodedBinarySecret;
+    return new Buffer(data.SecretBinary, 'base64').toString('ascii');
   }
 
 };
 
 exports.handler = async (event) => {
-  secret = await getSecret();
-  bot.once('ready', () => {
-    console.log('At the ready!!')
+  const botSecret = await getSecret('bot_client_secret');
+  const channelId = await getSecret('discord_channel_id');
+
+  bot.on('ready', () => {
+    console.log('At the ready!!');
+    bot.channels.cache.get(channelId).send('Tight.');
   });
 
-  bot.on("message", function (msg) {
-    if (msg.content === 'tight') {
-      msg.channel.send('tight.');
-      console.log("toight-ed " + msg.author.username);
-    }
-  });
+  bot.login(botSecret);
 
-  bot.login(secret);
+  await sleep(2000);
   return { statusCode: 200, body: JSON.stringify("Hello from AWS!") }
+}
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
