ec1427c44525e105454a5a44d4c4e193268a3ca8
Lauren <gorskilaurens@gmail.com>
2020-09-24 19:54:01 -0500

    refactor into src and clients dir

---
+++ packages/ec2/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('dotenv').config();
+
+const { discordListener } = require('./clients/discord');
+const { getSecret } = require('./security/secret');
+const {publishMessage} = require('./clients/sns');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send("Hello world we made it!");
+})
+
+app.get('/dyson', async (req, res) => {
+    secret = await getSecret();
+    const topic = discordListener(secret);
+    if (topic) {
+        await publishMessage(topic);
+    }
+    res.status(200).send("Hello from EC2!");
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
