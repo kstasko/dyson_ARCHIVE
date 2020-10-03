d5623aca24a57f405e126bad452cf4f21f395024
Lauren <gorskilaurens@gmail.com>
2020-10-03 18:49:31 -0500

    commit change for lerna

--- packages/ec2/src/index.js
+++ packages/ec2/src/index.js
@@ -21,4 +21,4 @@ app.get('/dyson', async (req, res) => {
     res.status(200).send("Hello from EC2!");
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log('Server running now on port 3000'));
