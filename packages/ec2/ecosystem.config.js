9750f2e30aaa97e4f9e39f30c21bf96329160bee
Lauren <gorskilaurens@gmail.com>
2020-10-03 20:08:00 -0500

    add pre deploy script

--- packages/ec2/ecosystem.config.js
+++ packages/ec2/ecosystem.config.js
@@ -9,7 +9,8 @@ module.exports = {
         host: 'ec2-18-223-33-56.us-east-2.compute.amazonaws.com',
         key: '~/.ssh/dyson_ec2.pem',
         ref: 'origin/master',
-        repo: 'git@github.com:kstasko/dyson_ec2.git',
+        repo: 'git@github.com:kstasko/dyson.git',
+        "pre-deploy-local" : "echo 'moving to ec2 dir' && cd packages/ec2",
         path: '/home/ubuntu/dyson_ec2',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
       }
