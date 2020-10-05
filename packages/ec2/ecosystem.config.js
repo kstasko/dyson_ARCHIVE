63a7d986379934e2acf69bd01d83daf3c8606802
Lauren <gorskilaurens@gmail.com>
2020-10-04 20:22:04 -0500

    deploy from master

--- packages/ec2/ecosystem.config.js
+++ packages/ec2/ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    apps: [{
-      name: 'dyson_ec2',
-      script: './src/index.js'
-    }],
-    deploy: {
-      production: {
-        user: 'ubuntu',
-        host: 'ec2-18-223-33-56.us-east-2.compute.amazonaws.com',
-        key: '~/.ssh/dyson_ec2.pem',
-        ref: 'origin/master',
-        repo: 'git@github.com:kstasko/dyson.git',
-        "pre-deploy-local" : "echo 'moving to ec2 dir' && cd packages/ec2",
-        path: '/home/ubuntu/dyson_ec2',
-        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
-      }
+  apps: [{
+    name: 'dyson_ec2',
+    script: './index.js',
+    cwd: '/home/ubuntu/dyson/packages/ec2/source/src'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-223-33-56.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/dyson_ec2.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:kstasko/dyson.git',
+      path: '/home/ubuntu/dyson_ec2',
+      'post-deploy': 'pwd && npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
+}
