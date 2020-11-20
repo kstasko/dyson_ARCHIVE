93682ed386eb4a8be8b504c646d0cde3885643f0
Lauren <gorskilaurens@gmail.com>
2020-11-19 19:38:06 -0600

    update paths and ec2 address

--- packages/ec2/ecosystem.config.js
+++ packages/ec2/ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   apps: [{
-    name: 'dyson_ec2',
+    name: 'dyson',
     script: './index.js',
-    cwd: '/home/ubuntu/dyson/packages/ec2/source/src'
+    cwd: '/home/ubuntu/dyson/current/packages/ec2/src'
   }],
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-223-33-56.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-223-30-124.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/dyson_ec2.pem',
       ref: 'origin/master',
       repo: 'git@github.com:kstasko/dyson.git',
-      path: '/home/ubuntu/dyson_ec2',
-      'post-deploy': 'pwd && npm install && pm2 startOrRestart ecosystem.config.js'
+      path: '/home/ubuntu/dyson',
+      'post-deploy': 'pwd && cd packages/ec2 && npm install && pm2 startOrRestart ./ecosystem.config.js'
     }
   }
 }
