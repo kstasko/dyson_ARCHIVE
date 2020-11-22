81b49625ece232d683547c294e4ceb84a551e20f
kevin <kevin.stasko@gmail.com>
2020-11-22 14:23:07 -0600

    dyson_wtf_react publish to SNS

--- packages/ec2/src/clients/discord.js
+++ packages/ec2/src/clients/discord.js
@@ -12,6 +12,10 @@ const discordListener = (secret) => {
             console.log("toight-ed " + msg.author.username);
             await publishMessage('dyson_tight');
         }
+        if (msg.content === 'wtf') {
+            console.log (msg.author.username + 'is reacting')
+            await publishMessage('dyson_wtf_reaction')
+        }
     });
 
     bot.login(secret);
