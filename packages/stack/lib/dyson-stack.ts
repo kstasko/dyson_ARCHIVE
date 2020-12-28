f134f2ec845be07fa0fc69408809819c17c12ae7
Lauren <gorskilaurens@gmail.com>
2020-12-27 19:23:28 -0600

    initial cdk commit

---
+++ packages/stack/lib/dyson-stack.ts
@@ -0,0 +1,29 @@
+import * as cdk from '@aws-cdk/core';
+import lambda = require('@aws-cdk/aws-lambda');
+import { Role, ServicePrincipal, IManagedPolicy, ManagedPolicy, PolicyStatement } from '@aws-cdk/aws-iam';
+import * as path from 'path';
+
+export class DysonStack extends cdk.Stack {
+  constructor(app: cdk.App, id: string, stackProps?: cdk.StackProps) {
+    super(app, id, stackProps);
+    const lambdaRole = new Role(this, 'lambdaRole',
+      {
+        assumedBy: new ServicePrincipal('lambda.amazon.com'),
+        managedPolicies: [ManagedPolicy.fromAwsManagedPolicyName('SecretsManagerReadWrite')]
+      }
+    )
+
+    new lambda.Function(this, 'dyson-tight-2', {
+      code: new lambda.InlineCode(path.join(__dirname, '..', '..', 'lambda', 'dyson-tight')),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      timeout: cdk.Duration.seconds(10),
+      role: lambdaRole
+    });
+
+  }
+}
+
+const app = new cdk.App();
+new DysonStack(app, 'DysonStack');
+app.synth();
