742a48e196ffa68c21bfbda7d1aa49e7a2add72e
stask_overflow <kstasko@users.noreply.github.com>
2021-03-28 15:11:49 -0500

    EC2 logic change
    EC2 changed to publish all messages to single SNS Topic

--- packages/ec2/src/clients/sns.js
+++ packages/ec2/src/clients/sns.js
@@ -1,18 +1,21 @@
 const { AWS } = require('./AWS/AWS');
-const AWS_ACCOUNT = 'arn:aws:sns:us-east-2:467222377375'
+const DYSON_SNS = 'arn:aws:sns:us-east-2:467222377375:dyson-message'
 
 
-async function publishMessage(topic) {
+async function publishMessage(message) {
     try {
-        console.log('SNS Trying to Send Message')
+        console.log('Publishing Message to SNS')
+
         const params = {
-            Message: 'hello!',
-            TopicArn: `${AWS_ACCOUNT}:${topic}`
+            Message: message,
+            TopicArn: `${DYSON_SNS}`
         }
 
         const awsSNSClient = new AWS.SNS()
         const data = await awsSNSClient.publish(params).promise();
-        console.log(`Message ${params.Message} Sent to Topic ${params.TopicArn} with id ${data.MessageId}`);
+
+        console.log(`Message ${params.Message} Sent to Topic ${params.DYSON_SNS} with id ${data.MessageId}`);
+
     } catch (err) {
         console.log('Error Publishing Message', err);
     }
