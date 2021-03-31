19ffbfe97501cafd99c64c166a2ae47c634dd7dc
Lauren <gorskilaurens@gmail.com>
2021-03-30 21:19:10 -0500

    dependency update

--- packages/lambda/dyson-message-director/src/index.js
+++ packages/lambda/dyson-message-director/src/index.js
@@ -1,11 +1,11 @@
-const SNS = require('./clients/sns');
+const { publishMessage } = require('./clients/sns');
 exports.handler = async (event) => {
     //sample SNS event https://docs.aws.amazon.com/lambda/latest/dg/with-sns.html
     const discordMessage = (JSON.parse(event.Records[0].SNS.Message)).content;
 
     if (discordMessage === 'tight') {
-        await SNS.publishMessage('tight');
+        await publishMessage('tight');
     } else if (discordMessage === 'wtf') {
-        await SNS.publishMessage('dyson-wtf-react');
+        await publishMessage('dyson-wtf-react');
     }
 }
