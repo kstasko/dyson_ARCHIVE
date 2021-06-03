ef789cfe93e40acc80c75ddbaba8e450b4a0e812
stask_overflow <kstasko@users.noreply.github.com>
2021-06-02 20:15:10 -0500

    for fun

--- packages/lambda/dyson-tight/index.js
+++ packages/lambda/dyson-tight/index.js
@@ -1,20 +1,21 @@
-const webhook = require("webhook-discord");
+const Discord = require('discord.js');
 
+const bot = new Discord.Client();
 var AWS = require('aws-sdk');
 const region = process.env.AWS_REGION;
 
 const client = new AWS.SecretsManager({
-  region: region
+    region: region
 });
 
 async function getSecret(secretName) {
-  const data = await client.getSecretValue({ SecretId: secretName }).promise();
-  if ('SecretString' in data) {
-    return JSON.parse(data.SecretString)[secretName];
-  }
-  else {
-    return new Buffer(data.SecretBinary, 'base64').toString('ascii');
-  }
+    const data = await client.getSecretValue({ SecretId: secretName }).promise();
+    if ('SecretString' in data) {
+        return JSON.parse(data.SecretString)[secretName];
+    }
+    else {
+        return new Buffer(data.SecretBinary, 'base64').toString('ascii');
+    }
 };
 
 exports.handler = async (event) => {
