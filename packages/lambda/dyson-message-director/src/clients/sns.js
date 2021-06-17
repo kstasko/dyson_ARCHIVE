1420b6d9554776e0f5d46df1de6040efe45588a6
stask_overflow <kstasko@users.noreply.github.com>
2021-06-16 20:28:25 -0500

    SNS message error fix attempt #12

--- packages/lambda/dyson-message-director/src/clients/sns.js
+++ packages/lambda/dyson-message-director/src/clients/sns.js
@@ -1,12 +1,12 @@
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-2'});
 
-async function publishMessage(topic) {
+async function publishMessage(topic. message) {
     try {
         console.log('Publishing Message to SNS')
 
         const params = {
-            Message: 'hello!',
+            Message: message,
             TopicArn: `arn:aws:sns:us-east-2:467222377375:${topic}`
         }
 
--- packages/lambda/dyson-message-director/src/index.js
+++ packages/lambda/dyson-message-director/src/index.js
@@ -1,14 +1,15 @@
 const { publishMessage } = require('./clients/sns');
 exports.handler = async (event) => {
     //sample SNS event https://docs.aws.amazon.com/lambda/latest/dg/with-sns.html
-    const discordMessage = (JSON.parse(event.Records[0].Sns.Message)).content;
+    console.log(event);
+    const message = event.Records[0].Sns.Message;
+    console.log(message);
 
     if (discordMessage === 'tight') {
-        await publishMessage('dyson-tight');
+        await publishMessage('dyson-tight', message);
     } else if (discordMessage === 'wtf') {
-        await publishMessage('dyson-wtf-react')
+        await publishMessage('dyson-wtf-react', message)
     } else if (discordMessage.substring(0, 3) === 'c4m') {
-        console.log(discordMessage);
-        await publishMessage('dyson-c4m');
+        await publishMessage('dyson-c4m', message);
     }
 }
