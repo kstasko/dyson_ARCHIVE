5f30a87501529f64bffc9428154fd961b0b2fd3a
stask_overflow <kstasko@users.noreply.github.com>
2021-06-16 20:33:40 -0500

    SNS message error fix attempt #13

--- packages/lambda/dyson-message-director/src/index.js
+++ packages/lambda/dyson-message-director/src/index.js
@@ -2,14 +2,14 @@ const { publishMessage } = require('./clients/sns');
 exports.handler = async (event) => {
     //sample SNS event https://docs.aws.amazon.com/lambda/latest/dg/with-sns.html
     console.log(event);
-    const message = event.Records[0].Sns.Message;
-    console.log(message);
+    const discordMessage = (JSON.parse(event.Records[0].Sns.Message)).content;
+    console.log(discordMessage);
 
     if (discordMessage === 'tight') {
-        await publishMessage('dyson-tight', message);
+        await publishMessage('dyson-tight', discordMessage);
     } else if (discordMessage === 'wtf') {
-        await publishMessage('dyson-wtf-react', message)
+        await publishMessage('dyson-wtf-react', discordMessage)
     } else if (discordMessage.substring(0, 3) === 'c4m') {
-        await publishMessage('dyson-c4m', message);
+        await publishMessage('dyson-c4m', discordMessage);
     }
 }
