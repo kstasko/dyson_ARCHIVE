897d34f00bf699095c7d18cc9f89c4f35b780c50
stask_overflow <kstasko@users.noreply.github.com>
2021-06-16 20:46:28 -0500

    SNS message error fix attempt #16

--- packages/lambda/dyson-message-director/src/clients/sns.js
+++ packages/lambda/dyson-message-director/src/clients/sns.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-2'});
 
-async function publishMessage(topic. message) {
+async function publishMessage(topic, message) {
     try {
         console.log('Publishing Message to SNS')
 
